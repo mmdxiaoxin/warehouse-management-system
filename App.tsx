87bbaf92e4b45f83518f6f4e61cd285491b181a6
小新 <782446723@qq.com>
2025-02-03 20:05:55 +0800

    refactor: 提取快捷导出

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {RealmProvider} from '@realm/react';
 import React from 'react';
-import {Cargo} from './models/Cargo';
-import {Category} from './models/Category';
-import {Model} from './models/Model';
-import {Record, RecordDetail, RecordDetailModel} from './models/Record';
-import {Unit} from './models/Unit';
+import {Cargo, Category, Model, Record, Unit} from './models';
+import {RecordDetail, RecordDetailModel} from './models/Record';
+
 import AppNavigator from './routes';
 
 function App(): React.JSX.Element {
---
+++ models/index.ts
@@ -0,0 +1,7 @@
+import {Unit} from './Unit';
+import {Record} from './Record';
+import {Model} from './Model';
+import {Category} from './Category';
+import {Cargo} from './Cargo';
+
+export {Cargo, Category, Model, Record, Unit};
